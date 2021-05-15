c4f774411dba7897be22ba0ad40c08c88b300589
YahyaKS <74717136+YahyaKS@users.noreply.github.com>
2021-05-15 21:59:40 +0300

    test

---
+++ Linkedin.js
@@ -0,0 +1,12 @@
+<nav >
+     <label for="" class="button">
+       <span class="fas fa-caret-down"></span>
+     </label>
+     <input type="checkbox" id="btn1">
+     <ul class="menu">
+       <li><a href="#">home</a></li>
+       <li><a href="#">home</a></li>
+       <li><a href="#">home</a></li>
+     </ul>
+</nav> 
+
